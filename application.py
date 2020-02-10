da51ddaf2eaa6482dc409aac2c0a4569dbd4fa12
Wynand Tredoux <wynand.tredoux@ucalgary.ca>
2020-02-09 23:20:58 -0700

    completed search page TODO: finish book page

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ DATABASE_URL=postgres://qbtxfingwrqmyc:962e9c32a97ff8eb6dff5e85a07032b9a9758d9da
 import os
 import hashlib
 
-from flask import Flask, session, request, render_template, redirect, url_for
+from flask import Flask, session, request, render_template, redirect, url_for, abort
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -17,6 +17,14 @@ def encrypt_string(hash_string):
     sha_signature = \
         hashlib.sha256(hash_string.encode()).hexdigest()
     return sha_signature
+
+
+# function to deal with quotes in SQL queries
+def SQLquotes(str):
+    if "'" in str:
+        return str.replace("'", "''")
+    else:
+        return str
 
 
 app = Flask(__name__)
@@ -30,34 +38,35 @@ app.config.from_pyfile('config.cfg', silent=True)  # load settings from config.c
 Session(app)
 
 # Set up database
-engine = create_engine(
-    'postgres://qbtxfingwrqmyc:962e9c32a97ff8eb6dff5e85a07032b9a9758d9da76c231864487642df21726a@ec2-35-168-54-239.compute-1.amazonaws.com:5432/d3tu5tucael6mf')
+engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
+
 
 @app.route("/")
 def home():
-    if session.get("user") is None:
+    if session.get("user") is None:  # initialize user to empty
         session["user"] = ''
     return render_template('home.html', user=session["user"])
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if session.get("user") is None:
+    if session.get("user") is None:  # initialize user to empty
         session["user"] = ''
     # normal page load
     if request.method == "GET":
         return render_template('login.html', user=session["user"])
+
     # login/register action
     else:
+        # get username and password for the login or registration forms
         username = request.form.get('Username')
         # escape ' character by replacing with ''
-        if "'" in username:
-            value = username.replace("'", "''")
+        username = SQLquotes(username)
         password = request.form.get('Password')
-        hashpass = encrypt_string(password) # password doesn't need any sanitation since it is hashed
-        if 'login' in request.form:
-            print('login!')
+        hashpass = encrypt_string(password)  # password doesn't need any sanitation since it is hashed
+
+        if 'login' in request.form:  # if login was pressed
             # find all users with username
             userswithname = db.execute("SELECT username,password FROM users WHERE username=:u AND password=:p",
                                        {"u": username, "p": hashpass}).fetchall()
@@ -65,14 +74,15 @@ def login():
             if len(userswithname) == 0:
                 return render_template('login.html', logError='Username or password is incorrect', user=session["user"])
             session["user"] = username
-            print("login success")
+            print(f"User {username} has logged in")
             return redirect(url_for('search'))
-        elif 'register' in request.form:
+
+        elif 'register' in request.form:  # if register was pressed
             print('register')
             # check if username is blank
             if username == '':
                 return render_template('login.html', regError='Username cannot be blank', user=session["user"])
-                # check if password is blank
+            # check if password is blank
             if password == '':
                 return render_template('login.html', regError='Password cannot be blank', user=session["user"])
             # check if username is already in database
@@ -82,6 +92,13 @@ def login():
                     # reload the page, displaying an error message
                     return render_template('login.html', regError='This username is already taken',
                                            user=session["user"])
+
+            # check if password matches confirmed password
+            checkpassword = request.form.get('ConfirmPassword')
+            if password != checkpassword:
+                return render_template('login.html', regError='Passwords must match',
+                                       user=session["user"])
+
             # register the user in the database with a hashed password
             db.execute("INSERT INTO users (username, password) VALUES (:u, :p)",
                        {"u": username, "p": hashpass})
@@ -113,14 +130,64 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route("/search")
+@app.route("/search", methods=["GET", "POST"])
 def search():
+    # redirect unlogged users to homepage
     if session.get("user") is None:
         return redirect(url_for('home'))
     elif session.get("user") == '':
         return redirect(url_for('home'))
-    return render_template('search.html', user=session["user"])
+    # get method
+    if request.method == "GET":
+        return render_template('search.html', user=session["user"])
 
+    # post method, get all form values
+    isbn = SQLquotes(request.form.get('isbn'))
+    title = SQLquotes(request.form.get('title'))
+    author = SQLquotes(request.form.get('author'))
+    year = request.form.get('year')
+    #print(f"{isbn}, {title}, {author}, {year}")
+
+    # build SQL query
+    command = ("SELECT * FROM books WHERE "
+               "isbn LIKE '%%%(isbn)s%%' AND "
+               "title LIKE '%%%(title)s%%' AND "
+               "author LIKE '%%%(author)s%%'" % {
+                   "isbn": isbn, 'title': title, "author": author})
+    # 'year' cannot use LIKE operator, so only include if the user submitted it
+    if year != '':
+        command = command + " AND year = '%(year)s'" % {'year': year}
+    #print(command)
+
+    # query the database for books
+    foundbooks = db.execute(command).fetchall()
+    #print(foundbooks)
+
+    return render_template('search.html', user=session["user"], numbooks=len(foundbooks), books=foundbooks)
+
+@app.route("/book", methods=["GET", "POST"])
+def book():
+    # redirect unlogged users to homepage
+    if session.get("user") is None:
+        return redirect(url_for('home'))
+    elif session.get("user") == '':
+        return redirect(url_for('home'))
+    # get request (coming from search page)
+    if request.method == "GET":
+        isbn = request.args.get('isbn', None)
+        title = request.args.get('title', None)
+        author = request.args.get('author', None)
+        year = request.args.get('year', None)
+
+        # if any of the above is none, give 404 page
+        if isbn is None or title is None or author is None or year is None:
+            abort(404)
+
+        print(f"Request info on: {isbn}, {title}, {author}, {year}")
+        return render_template('book.html', user=session["user"],isbn=isbn,title=title,author=author,year=year)
+    # post request (submitting a review)
+    #else:
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint
+
+db = SQLAlchemy()
+
+class Book(db.Model):
+    __tablename__ = "books"
+    isbn = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, CheckConstraint('year>0'), nullable=False)
+
+
+class Passenger(db.Model):
+    __tablename__ = "passengers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey("flights.id"), nullable=False)
