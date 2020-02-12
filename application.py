3eec6eb6c7f51f88d768fa29199c0d58c8d49a3f
Wynand Tredoux <wynand.tredoux@ucalgary.ca>
2020-02-11 21:24:44 -0700

    Added flask_run.bat for convenience in setting environment variables and running flask

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 """
 Environemnt variables:
+FLASK_APP=application.py
 DATABASE_URL=postgres://qbtxfingwrqmyc:962e9c32a97ff8eb6dff5e85a07032b9a9758d9da76c231864487642df21726a@ec2-35-168-54-239.compute-1.amazonaws.com:5432/d3tu5tucael6mf
 API_KEY=v6TC76FrNDwCdsxMwPBNg
 """
